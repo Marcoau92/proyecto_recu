84f793618f0187490d0c2f1482252ccc769f13c6
Marco Alvarez <marco.au92@gmail.com>
2021-12-20 17:28:35 -0300

    first commit

---
+++ tarea.py
@@ -0,0 +1,10 @@
+import numpy as np
+import jax.numpy as jnp
+import jax.random as random
+import numpyro
+import numpyro.distributions as dist
+
+def model(x, y=None):
+    raise NotImplementedError("Debe implementar esta función")
+
+
