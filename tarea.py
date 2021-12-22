1ded652cffca11e3a6de2bac015f66bb9aef9a0c
marcoau92 <marco.au92@gmail.com>
2021-12-22 19:02:38 -0300

    modelo3

--- tarea.py
+++ tarea.py
@@ -3,31 +3,36 @@ import jax.numpy as jnp
 import jax.random as random
 import numpyro
 import numpyro.distributions as dist
+from decimal import Decimal
+import scipy
 
 def model(x, y=None):
-    teta = []
-    teta00 = dist.Normal(loc=jnp.zeros(3), scale=5*jnp.ones(3)).to_event(1)
-    teta10 = numpyro.sample("teta00", teta00)
-    teta.append(teta10)
-    teta01 = dist.Normal(loc=jnp.zeros(3), scale=5*jnp.ones(3)).to_event(1)
-    teta11 = numpyro.sample("teta01", teta01)
-    teta.append(teta11)
-    teta02 = dist.Normal(loc=jnp.zeros(3), scale=5*jnp.ones(3)).to_event(1)
-    teta12 = numpyro.sample("teta02", teta02)
-    teta.append(teta12)
+    s = rutina_teta0(y)
+    d = rutina_teta1(y)
+    f = rutina_teta2(y)
+    teta = np.array([s,d,f]).astype(np.float)
     print(teta)
     lamda = []
     for i in range(197):
-        j = 1
-        temp = 0
-        for j in range(3):
-            temp = teta[j] + x[i,j]
-        lamdai = jnp.exp(teta[0] + temp)
+        temp0 = 0
+        temp1 = 0
+        temp2 = 0
+        temp0 = teta[0] + x[i,0]
+        temp1 = teta[1] + x[i,1]
+        temp2 = teta[2] + x[i,2]
+        lamdai = np.exp(teta[0] + temp0 + temp1 + temp2)
         lamda.append(lamdai)
     p = []
     for k in range(197):
-        aux = (lamda[i]**y[i])/np.math.factorial(y[i])
-        pk = aux*jnp.exp(-lamda[i])
+        #aux0 = Decimal((lamda[k]**y[k]))
+        #aux1 = Decimal(np.math.factorial(y[k]))
+        #print(y[k])
+        #print(lamda[k])
+        #print(aux0)
+        #print(aux1)
+        #aux = aux0/aux1
+        #pk = Decimal(aux*jnp.exp(-lamda[k]))
+        
         p.append(pk)
     return p
 
@@ -39,11 +44,11 @@ def run_mcmc_nuts(partial_model, x, y, rng_key_):
     """
     rng_key = random.PRNGKey(1234)
     rng_key, rng_key_ = random.split(rng_key)
-
+    print(rng_key_)
     
     sampler = numpyro.infer.MCMC(sampler=numpyro.infer.NUTS(partial_model), 
                              num_samples=1000, num_warmup=100, thinning=1,
-                             num_chains=2)
+                             num_chains=3)
 
     sampler.run(rng_key_, x, y)
 
@@ -51,7 +56,15 @@ def run_mcmc_nuts(partial_model, x, y, rng_key_):
     
     return sampler.get_samples()
 
-def rutina_teta():
+def rutina_teta0(y):
     teta00 = dist.Normal(loc=jnp.zeros(3), scale=5*jnp.ones(3)).to_event(1)
-    teta10 = numpyro.sample("teta00", teta00)
+    teta10 = numpyro.sample("teta00", dist.Poisson(teta00),obs= 1)
     return teta10
+def rutina_teta1(y):
+    teta01 = dist.Normal(loc=jnp.zeros(3), scale=5*jnp.ones(3)).to_event(1)
+    teta11 = numpyro.sample("teta01", dist.Poisson(teta01),obs= 1)
+    return teta11
+def rutina_teta2(y):
+    teta02 = dist.Normal(loc=jnp.zeros(3), scale=5*jnp.ones(3)).to_event(1)
+    teta12 = numpyro.sample("teta02", dist.Poisson(teta02),obs= 1)
+    return teta12
