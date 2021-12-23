66161a986e4cf784fd34070a6a4e5171d87f6e91
marcoau92 <marco.au92@gmail.com>
2021-12-23 20:33:14 -0300

    modelo6

--- tarea.py
+++ tarea.py
@@ -6,7 +6,10 @@ import numpyro.distributions as dist
 
 def model(x, y=None):
     prior_dist = dist.Normal(loc=jnp.zeros(4), scale=5*jnp.ones(4)).to_event(1) 
-    theta = rutina_theta0(prior_dist)
+    #theta = [rutina_theta0(x),rutina_theta1(x),rutina_theta2(x),rutina_theta3(x)]
+    theta = [0.5,0.2,0.3,0.7]
+    print(theta)
+    #theta = numpyro.sample("theta", prior_dist)
     s_eps = numpyro.sample("s", dist.HalfCauchy(scale=5.0))
     with numpyro.plate('datos', size=197):
         f = numpyro.deterministic('lambda',
@@ -33,18 +36,17 @@ def run_mcmc_nuts(partial_model, x, y, rng_key_):
     sampler.print_summary(prob=0.9)
     
     return sampler.get_samples()
-
-def rutina_theta0(prior_dist):
-    theta = numpyro.sample("theta", prior_dist)
+def rutina_theta0(x):
+    theta = numpyro.sample('theta0',dist.Normal(loc=0, scale=5),obs=1)
     return theta
-def rutina_theta1(y,s_eps):
-    theta01 = numpyro.sample('theta1',dist.Normal(loc=jnp.zeros(3), scale=s_eps*jnp.ones(3)),obs=1)
+def rutina_theta1(x):
+    theta01 = numpyro.sample('theta1',dist.Normal(loc=0, scale=5),obs=1)
     return theta01
-def rutina_theta2(y,s_eps):
-    theta02 = numpyro.sample('theta2',dist.Normal(loc=jnp.zeros(3), scale=s_eps*jnp.ones(3)),obs=1)
+def rutina_theta2(x):
+    theta02 = numpyro.sample('theta2',dist.Normal(loc=0, scale=5),obs=1)
     return theta02
-def rutina_theta3(y,s_eps):
-    theta03 = numpyro.sample('theta3',dist.Normal(loc=jnp.zeros(3), scale=s_eps*jnp.ones(3)),obs=1)
+def rutina_theta3(x):
+    theta03 = numpyro.sample('theta3',dist.Normal(loc=0, scale=5),obs=1)
     return theta03
 def plot_data(ax, x, y):
     ax.scatter(x[y == 0, 0], x[y == 0, 1], c='k', marker='o')
@@ -69,3 +71,38 @@ def get_predictive_samples(partial_model,
     else:
         raise ValueError("Debe entregarse mcmc_trace o num_samples")
     return predictive(rng_key_, x_test)
+
+def plot_predictive_posterior(ax, x1, x2, y, cmap, vmin=0, vmax=1, title=None):
+    cmap = ax.pcolormesh(x1,
+                         x2,
+                         y.reshape(len(x1), len(x2)),
+                         cmap=cmap,
+                         shading='gouraud',
+                         vmin=vmin,
+                         vmax=vmax)
+    plt.colorbar(cmap, ax=ax)
+    if title is not None:
+        ax.set_title(title)
+        
+def binary_mode(y):
+    p = np.mean(y, axis=0)
+    return p > 0.5
+
+
+def entropy(y):
+    p = np.mean(y, axis=0)
+    return -p * np.log(p + 1e-10)
+
+def plot_data(ax, x, y):
+    ax.scatter(x[y == 0, 0], x[y == 0, 1], c='k', marker='o')
+    ax.scatter(x[y == 1, 0], x[y == 1, 1], c='k', marker='x')
+    ax.set_xlabel('x[:, 0]')
+    ax.set_ylabel('x[:, 1]')
+    
+def autocorrelation(trace):
+    """
+    Retorna la autocorrelación de una traza
+    """
+    trace_norm = (trace - np.mean(trace)) / np.std(trace)
+    rho = np.correlate(trace_norm, trace_norm, mode='full')
+    return rho[len(rho) // 2:] / len(trace_norm)
