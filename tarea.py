51e1dcc16e2f949c5d54ebf8153d89afdbbd85fe
marcoau92 <marco.au92@gmail.com>
2021-12-23 12:22:34 -0300

    modelo5

--- tarea.py
+++ tarea.py
@@ -5,35 +5,14 @@ import numpyro
 import numpyro.distributions as dist
 
 def model(x, y=None):
-    #np.random.seed(1234)
-    s_eps = 5
-    s = rutina_theta0(y,s_eps)
-    #b_star, w_star, s_eps_star, N = 10, 2.5, 1., 10
-    #x1 = np.random.randn(N)
-    #y1 = b_star + w_star*x1 +  np.random.randn(N)*s_eps_star
-    d = rutina_theta1(y,s_eps)
-    f = rutina_theta2(y,s_eps)
-    g = rutina_theta3(y,s_eps)
-    theta = jnp.array([s,d,f,g]).astype(np.float)
-    p = []
-    
-    for i in range(197):
-        temp0 = theta[1] * x[i,0]
-        temp1 = theta[2] * x[i,1]
-        temp2 = theta[3] * x[i,2]
-        lamdai = jnp.exp(theta[0] + temp0 + temp1 + temp2)
-        p.append(lamdai)
-    lamda = jnp.array(p)
-    print(lamda.shape)
-    print(y.shape)
-    with numpyro.plate('datos', size=len(y)):
-        f = numpyro.deterministic('lamda', value=lamda)
-        print(f.shape)
-        #numpyro.sample('c', dist.Poisson(f), obs=y)
+    prior_dist = dist.Normal(loc=jnp.zeros(4), scale=5*jnp.ones(4)).to_event(1) 
+    theta = rutina_theta0(prior_dist)
+    s_eps = numpyro.sample("s", dist.HalfCauchy(scale=5.0))
+    with numpyro.plate('datos', size=197):
+        f = numpyro.deterministic('lambda',
+                            value=jnp.exp(theta[0] + theta[1]*x[0] + theta[2]*x[1] + theta[3]*x[2]))
+        numpyro.sample("y", dist.Poisson(f), obs=y)
         return f
-    #sam = numpyro.sample('y', dist.Poisson(lamda), obs=y)
-    #print(sam)
-    #return sam
 
 def run_mcmc_nuts(partial_model, x, y, rng_key_):
     """
@@ -41,23 +20,23 @@ def run_mcmc_nuts(partial_model, x, y, rng_key_):
     Utilice el algoritmo de muestreo No U-turn (NUTS)
     Nota: Puede agregar argumentos a la función si lo necesita
     """
-    rng_key = random.PRNGKey(1234)
-    rng_key, rng_key_ = random.split(rng_key)
-    print(rng_key_)
-    
-    sampler = numpyro.infer.MCMC(sampler=numpyro.infer.NUTS(partial_model), 
-                             num_samples=1000, num_warmup=100, thinning=1,
-                             num_chains=3)
+    rng_key, rngkey = random.split(rng_key)
 
-    sampler.run(rng_key_,x, y)
+    sampler = numpyro.infer.MCMC(sampler=numpyro.infer.NUTS(tarea.model), 
+                             num_samples=1000, num_warmup=100, thinning=1,
+                             num_chains=2)
+
+    sampler.run(rngkey, x, y)
+
+
 
     sampler.print_summary(prob=0.9)
     
     return sampler.get_samples()
 
-def rutina_theta0(y,s_eps):
-    theta00 = numpyro.sample('theta0',dist.Normal(loc=jnp.zeros(3), scale=s_eps*jnp.ones(3)),obs=1)
-    return theta00
+def rutina_theta0(prior_dist):
+    theta = numpyro.sample("theta", prior_dist)
+    return theta
 def rutina_theta1(y,s_eps):
     theta01 = numpyro.sample('theta1',dist.Normal(loc=jnp.zeros(3), scale=s_eps*jnp.ones(3)),obs=1)
     return theta01
@@ -67,3 +46,26 @@ def rutina_theta2(y,s_eps):
 def rutina_theta3(y,s_eps):
     theta03 = numpyro.sample('theta3',dist.Normal(loc=jnp.zeros(3), scale=s_eps*jnp.ones(3)),obs=1)
     return theta03
+def plot_data(ax, x, y):
+    ax.scatter(x[y == 0, 0], x[y == 0, 1], c='k', marker='o')
+    ax.scatter(x[y == 1, 0], x[y == 1, 1], c='k', marker='x')
+    ax.set_xlabel('x[:, 0]')
+    ax.set_ylabel('x[:, 1]')
+    
+def get_predictive_samples(partial_model,
+                           x_test,
+                           rng_key_,
+                           mcmc_trace=None,
+                           num_samples=None):
+    """
+    Retorna las muestras de la distribución predictiva
+    """
+    if mcmc_trace is not None:
+        predictive = numpyro.infer.Predictive(partial_model,
+                                              posterior_samples=mcmc_trace)
+    elif num_samples is not None:
+        predictive = numpyro.infer.Predictive(partial_model,
+                                              num_samples=num_samples)
+    else:
+        raise ValueError("Debe entregarse mcmc_trace o num_samples")
+    return predictive(rng_key_, x_test)
